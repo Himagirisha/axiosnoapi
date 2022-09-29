880eb5fac7ac19243b32a35af6b390322afe1d1f
Himagirisha Dharmaprakash <107013@tfcorp.com>
2022-09-29 22:19:20 +0530

    noapi

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <products-main />
+    <base-card mobileName="MotoRola g82 6gb ram"
+    rating="4" mobilePrice="14000"/>
+    <base-card mobileName="onePlus 7 pro 6gb ram"
+    rating="4.5" mobilePrice="49000"/>
+    
+  </div>
+</template>
+
+<script>
+import BaseCard from './components/BaseCard.vue';
+import ProductsMain from "./components/Products-main.vue";
+export default {
+  name: "App",
+  components: { ProductsMain, BaseCard },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mobileCard">
+    <h2>{{mobileName}}</h2>
+    <p>{{rating}}</p>
+    <h3>RS:{{mobilePrice}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["mobileName","rating","review","mobile"]
+}
+</script>
+
+<style>
+.mobileCard{
+    display: flex;
+    flex-direction: column;
+    border: 1px solid grey;
+    border-radius: 10px;
+    color: white;
+    background-color: blue;
+}
+</style>
